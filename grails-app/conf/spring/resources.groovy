d1bc485e527ff9afcafbab98b6c8e5c7eb4d7357
Tung Nguyen <nvntung@gmail.com>
2020-02-12 11:50:28 +0000

    Implement a solution to support CORS requests

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,9 +1,11 @@
 package spring
 
+import com.itersdesktop.javatechs.grails.CorsFilter
 import com.itersdesktop.javatechs.grails.auth.DefaultJsonPayloadCredentialsExtractor
 import com.itersdesktop.javatechs.grails.auth.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
+    corsFilter(CorsFilter)
     credentialsExtractor(DefaultJsonPayloadCredentialsExtractor)
 }
